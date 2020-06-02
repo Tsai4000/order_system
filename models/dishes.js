d1d33668c89d814eee7e6a987cd5cb593f880ab3
Tsai4000 <tsai4000nise+1@gmail.com>
2020-06-02 10:42:29 +0800

    Fix tab indent

--- models/dishes.js
+++ models/dishes.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const Dishes = new Schema({
-	dishes_name: { type: String, required: true, index: { unique: true } },
-	price: { type: Number, required: true }
+  dishes_name: { type: String, required: true, index: { unique: true } },
+  price: { type: Number, required: true }
 })
 
 module.exports = mongoose.model("dishes", Dishes)
