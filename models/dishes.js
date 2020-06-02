a6557d83532c0ae5168d369a16cc5a383c624c40
Tsai4000 <tsai4000nise+1@gmail.com>
2020-06-02 10:25:05 +0800

    Add dishes api

---
+++ models/dishes.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const Dishes = new Schema({
+	dishes_name: { type: String, required: true, index: { unique: true } },
+	price: { type: Number, required: true }
+})
+
+module.exports = mongoose.model("dishes", Dishes)
