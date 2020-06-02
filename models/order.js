1de9bb7a453ecefb0d2a2fc7e60f7f1b8a4db9de
Tsai4000 <tsai4000nise+1@gmail.com>
2020-06-02 15:45:58 +0800

    Fix order key name

--- models/order.js
+++ models/order.js
@@ -10,7 +10,7 @@ const Order = new Schema(
   {
     user_id: { type: String, required: true },
     username: { type: String, required: true },
-    dishes_name: { type: [DishesObject], required: true },
+    dishes_list: { type: [DishesObject], required: true },
     retrieve_time: { type: Number, required: true },
     special_needs: { type: String, required: false },
     completed: { type: String, require: true },
